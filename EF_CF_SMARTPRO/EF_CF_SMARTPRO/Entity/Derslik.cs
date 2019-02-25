99cc8f2f68cf7316eb6b1a9d11a69be15f0ba499
mehmetdemir <mail.mehmetdemir@gmail.com>
2019-02-25 12:54:22 +0300

    25.02.2019 Derste anlatılanlar. Form kısmı eksik.

---
+++ EF_CF_SMARTPRO/EF_CF_SMARTPRO/Entity/Derslik.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_CF_SMARTPRO.Entity
+{
+     public enum Yerleskeler
+     {
+          Kadikoy,
+          Sancaktepe
+     }
+     public class Derslik
+     {
+          public int DerslikID { get; set; }
+          public Yerleskeler DerslikYerleskesi { get; set; }
+          public byte DerslikKati { get; set; }
+          public string DerslikAdi { get; set; }
+
+          // Sinif sınıfından SinifDerslikID isimli özelliği ForeignKey yap
+          [ForeignKey("SinifDerslikID")]
+          public ICollection<Sinif> DerslikSiniflari { get; set; }
+
+     }
+}
---
+++ EF_CF_SMARTPRO/EF_CF_SMARTPRO/Entity/Ogretmen.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_CF_SMARTPRO.Entity
+{
+     [Table("Ogretmenler")]
+     public class Ogretmen
+     {
+          // Manuel PK tanımlama. Identity otomatik eklenir.
+          [Key]
+          public int OgretmenSiraNo { get; set; }
+
+          // Default Primary Key
+          //public int OgretmenID { get; set; }
+
+          [Required]
+          [Column("TC", TypeName = "varchar",Order = 6)]
+          public string OgretmenTcKimlik { get; set; }
+
+          [Index("INDEX_OgretmenAdi", IsClustered = false, IsUnique = false)]
+          [MaxLength(30)] // ideksleme sınırını aşmasın(8000 byte)
+          public string OgretmenAdi { get; set; }
+
+          public DateTime OgretmenIsBasiTarihi { get; set; }
+          public DateTime OgretmenIsFesihTarihi { get; set; }
+          public bool OgretmenAktifMi { get; set; }
+
+          [Timestamp]
+          public byte[] OgretmenSonDegisiklik { get; set; }
+
+
+          // Veri tabanında böyle bir kolon oluşturma.
+          [NotMapped]
+          public string HavadanOzellik { get; set; }
+
+          public ICollection<Sinif> OgretmenSiniflari { get; set; }
+     }
+}
